4ff043b1531d16558e4e88dce32e366ea6674448
Wren <ljleighfield@gmail.com>
2019-10-22 13:18:45 +0100

    js comments

--- main.js
+++ main.js
@@ -1,28 +1,20 @@
 
-/*Constants*/
-
+//Constants
+//Pushes the key id's into the notes array
 const keys = ['c-key', 'csharp-key', 'd-key', 'dsharp-key', 'e-key', 'f-key', 'fsharp-key', 'g-key', 'gsharp-key', 'a-key', 'asharp-key', 'b-key', 'chigh-key'];
 const notes = [];
 keys.forEach(function(key){
     notes.push(document.getElementById(key));
   })
-
+/*Changes background colour of key to indicate key has been pressed*/
 const keyDown = keys => {
     keys.target.style.backgroundColor = 'green'
 }
 const keyUp = keys => {
     keys.target.style.backgroundColor = ''
 }
+/*KeyPress uses pointer instead of mouse to allow finger input on touchscreen laptops*/
 const keyPress = note => {
-    /*note.onmousedown = () => {
-        keyDown(event);
-    }
-    note.onmouseup = () => {
-        keyUp(event);
-    }
-    note.onmouseout =() => {
-        keyUp(event);
-    }*/
     note.onpointerdown = () => {
         keyDown(event);
     }
