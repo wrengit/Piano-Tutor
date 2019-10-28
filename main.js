12cb07f52d2a54a3cd37902289c214f626f6c07e
Wren <ljleighfield@gmail.com>
2019-10-28 10:45:15 +0000

    Add note display when key pressed.

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 
 //Constants
-//Pushes the key id's into the notes array
-const keys = ['c-key', 'csharp-key', 'd-key', 'dsharp-key', 'e-key', 'f-key', 'fsharp-key', 'g-key', 'gsharp-key', 'a-key', 'asharp-key', 'b-key', 'chigh-key'];
-const notes = [];
-keys.forEach(function(key){
+/*Pushes the key id's into the notes array*/
+const keys = ['c-key', 'csharp-key', 'd-key', 'dsharp-key', 'e-key', 'f-key', 'fsharp-key', 'g-key', 'gsharp-key', 'a-key', 'asharp-key', 'b-key', 'chigh-key']
+const notes = []
+
+keys.forEach((key) => {
     notes.push(document.getElementById(key));
-  })
-/*Changes background colour of key to indicate key has been pressed*/
-const keyDown = keys => {
-    keys.target.style.backgroundColor = 'green'
+})
+/*Asigns #notebox to noteDataBox*/
+const noteDataBox = document.getElementById("notebox");
+
+/*Changes background colour of key to indicate key has been pressed, displays played note above piano*/
+const keyDown = key => {
+    key.target.style.backgroundColor = 'green';
+    noteDataBox.innerHTML = key.target.getAttribute("data-note");
 }
-const keyUp = keys => {
-    keys.target.style.backgroundColor = ''
+const keyUp = key => {
+    key.target.style.backgroundColor = ''
 }
-/*KeyPress uses pointer instead of mouse to allow finger input on touchscreen laptops*/
+
+/*keyPress uses pointer instead of mouse to allow finger input on touchscreen laptops*/
+
 const keyPress = note => {
     note.onpointerdown = () => {
         keyDown(event);
@@ -24,7 +31,7 @@ const keyPress = note => {
     note.onpointerleave = () => {
         keyUp(event);
     }
-};
-
+}
 notes.forEach(keyPress);
 
+/*Stored song arrays*/
