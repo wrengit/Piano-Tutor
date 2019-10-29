362234f73b4ec3551471472a9535e8c3ce2f7fbb
Wren <ljleighfield@gmail.com>
2019-10-29 08:40:48 +0000

    Add timer delay to displayed note

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
-
-//Constants
-/*Pushes the key id's into the notes array*/
+/*Pushes the key id's into the notes array, making all paino key divs available to notes array*/
 const keys = ['c-key', 'csharp-key', 'd-key', 'dsharp-key', 'e-key', 'f-key', 'fsharp-key', 'g-key', 'gsharp-key', 'a-key', 'asharp-key', 'b-key', 'chigh-key']
 const notes = []
 
 keys.forEach((key) => {
     notes.push(document.getElementById(key));
 })
-/*Asigns #notebox to noteDataBox*/
+/*Assigns #notebox to noteDataBox*/
 const noteDataBox = document.getElementById("notebox");
 
-/*Changes background colour of key to indicate key has been pressed, displays played note above piano*/
+/*Changes background colour of key to indicate key has been pressed, displays played note above piano on keyUp/Down variable*/
+let timerNote;
+function timerNoteDelay() { 
+    clearTimeout(timerNote);
+    timerNote = setTimeout (() => {
+        noteDataBox.innerHTML = ""
+    }, 1500)
+};
+
 const keyDown = key => {
     key.target.style.backgroundColor = 'green';
     noteDataBox.innerHTML = key.target.getAttribute("data-note");
 }
 const keyUp = key => {
-    key.target.style.backgroundColor = ''
+    key.target.style.backgroundColor = ''; 
+    timerNoteDelay();
 }
 
-/*keyPress uses pointer instead of mouse to allow finger input on touchscreen laptops*/
+/*Assigns the keyDown and keyUp variables to pointer events and makes them available to all piano keys. keyPress uses pointer instead of mouse to allow finger input on touchscreen laptops*/
 
 const keyPress = note => {
     note.onpointerdown = () => {
@@ -33,5 +40,3 @@ const keyPress = note => {
     }
 }
 notes.forEach(keyPress);
-
-/*Stored song arrays*/
